84794ff5ed8bd3f15680630703858f4be6101ed8
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-03-30 00:31:28 -0500

    Creating some articles and creating validations for invalid EndPoints

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import ArticleContent from "./ArticleContent";
 
 const Article = () => {
+    const { name } = useParams();
+    const article = ArticleContent.find((article) => article.name === name);
+    if (!article) {
+        return <h1>Article Does not exists</h1>
+    }
     return (
         <div>
             <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
-                Article
+                {article.title}
             </h1>
+            {article.content.map((paragraph, index) => (
+                <p className="mx-auto leading-relaxed text-base mb-4" key={index}>{paragraph}</p>
+            ))}
         </div>
     )
 }
