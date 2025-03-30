dc5c90bdc6411e043848da72d0c609f049d6a619
StevenBalagueraP <161085601+StevenBalagueraP@users.noreply.github.com>
2025-03-30 17:53:43 -0500

    Creating article views and adding other articles references

--- src/pages/ArticlesList.jsx
+++ src/pages/ArticlesList.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Articles from "../components/Articles";
+import articleContent from "./ArticleContent.jsx";
 
 const ArticlesList = () => {
     return (
@@ -6,6 +8,11 @@ const ArticlesList = () => {
             <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
                 Articles
             </h1>
+            <div className="container py-4 mx-auto">
+                <div className="flex flex-wrap -m-4">
+                    <Articles articles={articleContent}/>
+                </div>
+            </div>
         </div>
     )
 }
